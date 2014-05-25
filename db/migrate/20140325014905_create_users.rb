534aa308ba938486c88552a20fbb64ed50f5130c
Scott Mielcarski <scott_mielcarski@me.com>
2014-05-24 19:11:31 -0700

    [Add] Remeber me functionality to the user model.

--- db/migrate/20140325014905_create_users.rb
+++ db/migrate/20140325014905_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :password
       t.string :password_salt
+      t.string :authentication_token
 
       t.timestamps
     end
