9d7d87a50a848b3d0903fec13e52329ef43f0e68
Scott Mielcarski <scott_mielcarski@me.com>
2014-05-24 19:17:27 -0700

    [Add] AngularJS content editable directive to simplify handling content editable DOM elements.

---
+++ app/assets/javascripts/angular/directives/contentEditableDir.js
@@ -0,0 +1,25 @@
+bugtracker.directive('contenteditable', function() {
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: '?ngModel', // get a hold of NgModelController
+    link: function(scope, element, attrs, ngModel) {
+      if(!ngModel) return; // do nothing if no ng-model
+
+      // Specify how UI should be updated
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || '');
+      };
+
+      // Listen for change events to enable binding
+      element.on('blur keyup change', function() {
+        scope.$apply(read);
+      });
+
+      // Write data to the model
+      function read() {
+        var val = element.text();
+        ngModel.$setViewValue(val);
+      }
+    }
+  };
+});
