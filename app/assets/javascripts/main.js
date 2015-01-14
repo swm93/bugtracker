76fe0476538ec221323fdc461b5e2fb833cc2ba7
Scott Mielcarski <scott_mielcarski@me.com>
2015-01-14 11:08:10 -0800

    [Change] Large refactor of main.js; proper comments were added.
    Functionality remains the same, but configuration data has been decouple from the code.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,207 +1,17 @@
-bugtracker = angular.module('bugtracker', ['ngResource', 'ui.router', 'ngProgressLite']);
+/*/     MAIN
+ *  This is the main entry point to the application. All one time configuration
+ *  should be done here. The main function (run) also resides within this file.
+/*/
 
-bugtracker.config(['$stateProvider', '$httpProvider', '$locationProvider', 'ngProgressLiteProvider', function($stateProvider, $httpProvider, $locationProvider, ngProgressLiteProvider) {
-    $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = $("meta[name=\"csrf-token\"]").attr("content");
+
+window.Bugtracker = window.Bugtracker || {};
+Bugtracker.Config = Bugtracker.Config || {};
 
 
-    $stateProvider.state('home', {
-        url: '',
-        templateUrl: '/assets/layouts/home.html',
-        controller: 'HomeCtrl'
-    });
-
-    $stateProvider.state('users', {
-        abstract: true,
-        url: '/users',
-        templateUrl: '/assets/layouts/users.html',
-        controller: 'UserCtrl'
-    });
-    $stateProvider.state('users.new', {
-        url: '/signup',
-        templateUrl: '/assets/users/new.html',
-        controller: 'UserNewCtrl'
-    });
-    $stateProvider.state('users.login', {
-        url: '/login',
-        templateUrl: '/assets/users/login.html',
-        controller: 'UserLoginCtrl'
-    });
-    $stateProvider.state('users.logout', {
-        url: '/logout',
-        controller: 'UserLogoutCtrl',
-        resolve: {
-            user: resolve.user.logout
-        }
-    });
-
-    $stateProvider.state('projects', {
-        abstract: true,
-        url: '/projects',
-        templateUrl: '/assets/layouts/projects.html',
-        controller: 'ProjectCtrl',
-        data: {
-            breadcrumb: 'Projects',
-            breadcrumbRoute: 'projects.index'
-        }
-    });
-    $stateProvider.state('projects.single', {
-        abstract: true,
-        url: '/{projectId:[0-9]{1,8}}',
-        template: '<ui-view />',
-        resolve: {
-            project: resolve.project.findById,
-            bugs: resolve.bug.all,
-            permissions: resolve.permission.all
-        },
-        data: {
-            breadcrumb: '{{ projectId }}',
-            breadcrumbRoute: 'projects.single.show'
-        }
-    });
-    $stateProvider.state('projects.index', {
-        url: '',
-        templateUrl: '/assets/projects/index.html',
-        controller: 'ProjectIndexCtrl',
-        resolve: {
-            projects: resolve.project.all
-        },
-        data: {
-            breadcrumb: undefined
-        }
-    });
-    $stateProvider.state('projects.single.show', {
-        url: '',
-        templateUrl: '/assets/projects/show.html',
-        controller: 'ProjectShowCtrl',
-        data: {
-            breadcrumb: undefined
-        }
-    });
-    $stateProvider.state('projects.new', {
-        url: '/new',
-        templateUrl: '/assets/projects/new.html',
-        controller: 'ProjectNewCtrl',
-        data: {
-            breadcrumb: 'New'
-        }
-    });
-    $stateProvider.state('projects.single.edit', {
-        url: '/edit',
-        templateUrl: '/assets/projects/edit.html',
-        controller: 'ProjectEditCtrl',
-        data: {
-            breadcrumb: 'Edit'
-        }
-    });
-
-    $stateProvider.state('projects.single.bugs', {
-        abstract: true,
-        url: '/bugs',
-        templateUrl: '/assets/layouts/bugs.html',
-        controller: 'BugCtrl',
-        data: {
-            breadcrumb: 'Bugs',
-            breadcrumbRoute: 'projects.single.bugs.index'
-        }
-    });
-    $stateProvider.state('projects.single.bugs.single', {
-        abstract: true,
-        url: '/{bugId:[0-9]{1,8}}',
-        template: '<ui-view />',
-        resolve: {
-            bug: resolve.bug.findById
-        },
-        data: {
-            breadcrumb: '{{ bugId }}',
-            breadcrumbRoute: 'projects.single.bugs.single.show'
-        },
-    });
-    $stateProvider.state('projects.single.bugs.index', {
-        url: '',
-        templateUrl: '/assets/bugs/index.html',
-        controller: 'BugIndexCtrl',
-        data: {
-            breadcrumb: undefined
-        }
-    });
-    $stateProvider.state('projects.single.bugs.single.show', {
-        url: '',
-        templateUrl: '/assets/bugs/show.html',
-        controller: 'BugShowCtrl',
-        data: {
-            breadcrumb: undefined
-        }
-    });
-    $stateProvider.state('projects.single.bugs.new', {
-        url: '/new',
-        templateUrl: '/assets/bugs/new.html',
-        controller: 'BugNewCtrl',
-        data: {
-            breadcrumb: 'New'
-        }
-    });
-    $stateProvider.state('projects.single.bugs.single.edit', {
-        url: '/edit',
-        templateUrl: '/assets/bugs/edit.html',
-        controller: 'BugEditCtrl',
-        data: {
-            breadcrumb: 'Edit'
-        }
-    });
-    $stateProvider.state('projects.single.bugs.feed', {
-        url: '/feed',
-        templateUrl: '/assets/bugs/feed.html',
-        controller: 'BugFeedCtrl',
-        data: {
-            breadcrumb: 'Feed'
-        }
-    });
-
-    $stateProvider.state('error', {
-        abstract: true,
-        templateUrl: '/assets/layouts/errors.html',
-        controller: 'ErrorCtrl'
-    });
-    $stateProvider.state('error.401', {
-        url: '/401',
-        templateUrl: '/assets/errors/401.html',
-        controller: 'Error401Ctrl'
-    });
-    $stateProvider.state('error.404', {
-        url: '/404',
-        templateUrl: '/assets/errors/404.html',
-        controller: 'Error404Ctrl'
-    });
-}]);
-
-bugtracker.run(['$rootScope', '$state', 'ngProgressLite', 'User', function($rootScope, $state, ngProgressLite, User) {
-    //set the current user if a session is still in progress
-    User.currentUser(function(successData) {
-        if (successData.user) {
-            $rootScope.currentUser = successData.user;
-        }
-    }, function(errorData) {});
-
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-        ngProgressLite.start();
-    });
-    $rootScope.$on('$stateChangeSuccess', function() {
-        ngProgressLite.done();
-    });
-
-    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-        if (error.status === 401) {
-            $state.transitionTo('users.login');
-        }
-        else {
-            $state.transitionTo('error.' + error.status);
-        }
-        
-        ngProgressLite.done();
-    });
-}]);
-
-var resolve = {
+/*/     Routes Config
+ *  Map route names to data required to create them
+/*/
+Bugtracker.Config.resolve = {
     user: {
         logout: function(User, $q, $rootScope) {
             var deferred = $q.defer();
@@ -258,7 +68,7 @@ var resolve = {
         all: function(Permission, $q, $stateParams) {
             var deferred = $q.defer();
             Permission.all($stateParams.projectId, function(successData) {
-                deferred.resolve(successData); 
+                deferred.resolve(successData.permissions); 
             }, function(errorData) {
                 deferred.reject(errorData);
             });
@@ -267,7 +77,7 @@ var resolve = {
         findById: function(Permission, $q, $stateParams) {
             var deferred = $q.defer();
             Permission.findById($stateParams.permissionId, $stateParams.projectId, function(successData) {
-                deferred.resolve(successData); 
+                deferred.resolve(successData.permission); 
             }, function(errorData) {
                 deferred.reject(errorData);
             });
@@ -275,3 +85,268 @@ var resolve = {
         }
     },
 };
+
+Bugtracker.Config.routes = {
+    //  Home Routes  //
+    'home': {
+        url: '',
+        templateUrl: '/assets/layouts/home.html',
+        controller: 'HomeCtrl'
+    },
+    //  Error Routes  //
+    'error': {
+        abstract: true,
+        templateUrl: '/assets/layouts/errors.html',
+        controller: 'ErrorCtrl'
+    },
+    'error.401': {
+        url: '/401',
+        templateUrl: '/assets/errors/401.html',
+        controller: 'Error401Ctrl'
+    },
+    'error.404': {
+        url: '/404',
+        templateUrl: '/assets/errors/404.html',
+        controller: 'Error404Ctrl'
+    },
+    //  User Routes  //
+    'users': {
+        abstract: true,
+        url: '/users',
+        templateUrl: '/assets/layouts/users.html',
+        controller: 'UserCtrl'
+    },
+    'users.new': {
+        url: '/signup',
+        templateUrl: '/assets/users/new.html',
+        controller: 'UserNewCtrl'
+    },
+    'users.login': {
+        url: '/login',
+        templateUrl: '/assets/users/login.html',
+        controller: 'UserLoginCtrl'
+    },
+    'users.logout': {
+        url: '/logout',
+        controller: 'UserLogoutCtrl',
+        resolve: {
+            user: Bugtracker.Config.resolve.user.logout
+        }
+    },
+    //  Project Routes  //
+    'projects': {
+        abstract: true,
+        url: '/projects',
+        templateUrl: '/assets/layouts/projects.html',
+        controller: 'ProjectCtrl',
+        data: {
+            breadcrumb: 'Projects',
+            breadcrumbRoute: 'projects.index'
+        }
+    },
+    'projects.single': {
+        abstract: true,
+        url: '/{projectId:[0-9]{1,8}}',
+        template: '<div ui-view />',
+        resolve: {
+            project: Bugtracker.Config.resolve.project.findById,
+            bugs: Bugtracker.Config.resolve.bug.all,
+            permissions: Bugtracker.Config.resolve.permission.all
+        },
+        data: {
+            breadcrumb: '{{ projectId }}',
+            breadcrumbRoute: 'projects.single.show'
+        }
+    },
+    'projects.index': {
+        url: '',
+        templateUrl: '/assets/projects/index.html',
+        controller: 'ProjectIndexCtrl',
+        resolve: {
+            projects: Bugtracker.Config.resolve.project.all
+        },
+        data: {
+            breadcrumb: undefined
+        }
+    },
+    'projects.single.show': {
+        url: '',
+        templateUrl: '/assets/projects/show.html',
+        controller: 'ProjectShowCtrl',
+        data: {
+            breadcrumb: undefined
+        }
+    },
+    'projects.new': {
+        url: '/new',
+        templateUrl: '/assets/projects/new.html',
+        controller: 'ProjectNewCtrl',
+        data: {
+            breadcrumb: 'New'
+        }
+    },
+    'projects.single.edit': {
+        url: '/edit',
+        templateUrl: '/assets/projects/edit.html',
+        controller: 'ProjectEditCtrl',
+        data: {
+            breadcrumb: 'Edit'
+        }
+    },
+    //  Bug Routes  //
+    'projects.single.bugs': {
+        abstract: true,
+        url: '/bugs',
+        templateUrl: '/assets/layouts/bugs.html',
+        controller: 'BugCtrl',
+        data: {
+            breadcrumb: 'Bugs',
+            breadcrumbRoute: 'projects.single.bugs.index'
+        }
+    },
+    'projects.single.bugs.single': {
+        abstract: true,
+        url: '/{bugId:[0-9]{1,8}}',
+        template: '<div ui-view />',
+        resolve: {
+            bug: Bugtracker.Config.resolve.bug.findById
+        },
+        data: {
+            breadcrumb: '{{ bugId }}',
+            breadcrumbRoute: 'projects.single.bugs.single.show'
+        }
+    },
+    'projects.single.bugs.index': {
+        url: '',
+        templateUrl: '/assets/bugs/index.html',
+        controller: 'BugIndexCtrl',
+        data: {
+            breadcrumb: undefined
+        }
+    },
+    'projects.single.bugs.single.show': {
+        url: '',
+        templateUrl: '/assets/bugs/show.html',
+        controller: 'BugShowCtrl',
+        data: {
+            breadcrumb: undefined
+        }
+    },
+    'projects.single.bugs.new': {
+        url: '/new',
+        templateUrl: '/assets/bugs/new.html',
+        controller: 'BugNewCtrl',
+        data: {
+            breadcrumb: 'New'
+        }
+    },
+    'projects.single.bugs.single.edit': {
+        url: '/edit',
+        templateUrl: '/assets/bugs/edit.html',
+        controller: 'BugEditCtrl',
+        data: {
+            breadcrumb: 'Edit'
+        }
+    },
+    'projects.single.bugs.feed': {
+        url: '/feed',
+        templateUrl: '/assets/bugs/feed.html',
+        controller: 'BugFeedCtrl',
+        data: {
+            breadcrumb: 'Feed'
+        }
+    }
+};
+
+/*/     Flash Alerts Config
+ *  Map flashProvider class name object keys to HTML class names
+/*/
+Bugtracker.Config.flashAlerts = {
+    successClassnames: 'alert-success',
+    infoClassnames: 'alert-info',
+    warnClassnames: 'alert-warning',
+    errorClassnames: 'alert-danger'
+};
+
+
+/*/     Bugtracker Module
+ *  Define the bugtracker module. This is the core object of the applicaiton.
+/*/
+bugtracker = angular.module('bugtracker', ['ngResource', 'ui.router', 'ui.select', 'ngSanitize', 'angular-flash.service', 'angular-flash.flash-alert-directive', 'ngProgressLite']);
+
+/*/     Config
+ *  Perform required setup for various providers:
+ *  - configure routes
+ *  - set csrf-token
+ *  - configure flash alerts
+/*/
+bugtracker.config([
+    '$stateProvider',
+    '$httpProvider',
+    '$locationProvider',
+    'flashProvider',
+    'ngProgressLiteProvider',
+
+    function($stateProvider, $httpProvider, $locationProvider, flashProvider, ngProgressLiteProvider) {
+    // Set the CSRF token on the $httpProvider object by parsing the DOM to find
+    // the CSRF token set by Rails. It's found in the content attribute of a
+    // meta tag with the name "csrf-token".
+    $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = $("meta[name=\"csrf-token\"]").attr("content");
+
+    // Set flash HTML classnames on the flashProvider object. Class names are
+    // mapped in the Bugtracker.Config.flashAlerts object.
+    $.each(Bugtracker.Config.flashAlerts, function(type, className) {
+        flashProvider[type].push(className);
+    });
+
+    // Set the routes on the $stateProvider object. Routes are defined within
+    // the Bugtracker.Config.routes object.
+    $.each(Bugtracker.Config.routes, function(name, options) {
+        $stateProvider.state(name, options);
+    });
+}]);
+
+
+/*/     Run
+ *  Perform required setup once application begins:
+ *  - set session if one is in progress
+ *  - define errors
+ *  - setup loading indicator
+/*/
+bugtracker.run([
+    '$rootScope', 
+    '$state',
+    'ngProgressLite',
+    'User',
+
+    function($rootScope, $state, ngProgressLite, User) {
+    // Set the user on the $rootScope object if a session is in progress.
+    User.currentUser(function(successData) {
+        if (successData.user) {
+            $rootScope.currentUser = successData.user;
+        }
+    }, function(errorData) {});
+
+    // Setup the loading indicator:
+    // - state change begins    ->  start the loading indicator
+    // - state change completes ->  stop the loading indicator
+    // - state change fails     ->  stop the loading indictor and display error
+    $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+        ngProgressLite.start();
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function(e, toState, toParams, fromState, fromParams) {
+        ngProgressLite.done();
+    });
+
+    $rootScope.$on('$stateChangeError', function(e, toState, toParams, fromState, fromParams, error) {
+        switch (error.status) {
+            case 401:
+                $state.transitionTo('users.login');
+            default:
+                $state.transitionTo('error.' + error.status);
+        }
+        
+        ngProgressLite.done();
+    });
+}]);
