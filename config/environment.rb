08676adc7c062ece36af4df3c82b220a1fce37c1
Scott Mielcarski <scott_mielcarski@me.com>
2015-01-14 11:13:42 -0800

    [Add] Favicon, iOS home screen links, and Windows tiles now work as expected.
    Favicon was generated using http://realfavicongenerator.net/. This website generates all assets, HTML, and configuration files in order to support favicons, iOS, and Windows properly. The HTML was refactored into helper functions within the application_helper.rb file.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Bugtracker::Application.initialize!
+
+Bugtracker::Application.configure do
+  config.app_name = "BugTracker"
+  config.windows_tile_color = "#00a300"
+end
