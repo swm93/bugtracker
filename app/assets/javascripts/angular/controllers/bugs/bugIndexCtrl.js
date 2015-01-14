cd655f4d498c1b2512c7697478e2cf9ba4cd1e75
Scott Mielcarski <scott_mielcarski@me.com>
2015-01-14 11:42:07 -0800

    [Add] Show tooltip for bugs index add button.

--- app/assets/javascripts/angular/controllers/bugs/bugIndexCtrl.js
+++ app/assets/javascripts/angular/controllers/bugs/bugIndexCtrl.js
@@ -7,6 +7,8 @@ bugtracker.controller('BugIndexCtrl', ['$scope', '$stateParams', '$filter', 'bug
     };
     $scope.sort = ['id'];
 
+    
+    $("[data-toggle='tooltip']").tooltip();
 
     $scope.hasVisibleBugs = function() {
         return $filter('filterMultiple')($scope.bugs, $scope.filter).length;
