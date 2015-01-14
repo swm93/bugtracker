f02543312d053f159596214bc04c3a81dce37936
Scott Mielcarski <scott_mielcarski@me.com>
2015-01-14 11:43:31 -0800

    [Remove] Delete unused project definition within bugCtrl.js.

--- app/assets/javascripts/angular/controllers/bugs/bugCtrl.js
+++ app/assets/javascripts/angular/controllers/bugs/bugCtrl.js
@@ -1,3 +1,1 @@
-bugtracker.controller('BugCtrl', ['$scope', '$stateParams', 'Project', function($scope, $stateParams, Project) {
-    $scope.project = Project.findById($stateParams.projectId);
-}]);
+bugtracker.controller('BugCtrl', ['$scope', '$stateParams', 'Project', function($scope, $stateParams, Project) {}]);
