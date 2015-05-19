d161d7300905d394be50d58cea38be490abe0c05
Scott Mielcarski <scott_mielcarski@me.com>
2015-05-18 17:03:04 -0700

    Add logo directive to support styling of an SVG image

---
+++ app/assets/javascripts/angular/directives/logoDir.js
@@ -0,0 +1,16 @@
+bugtracker.directive('logo', function() {
+    return {
+        restrict: 'E', // only activate on element attribute
+        templateUrl: '/assets/isometric-cube.svg',
+        link: function(scope, element, attrs) {
+            var height = element.height();
+            var width = element.height();
+            var min = Math.min(height, width);
+
+            element.css({
+                width: min,
+                height: min
+            });
+        }
+    };
+});
