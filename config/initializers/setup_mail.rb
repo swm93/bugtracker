edca0a4e339b034f622b7d1425c67a56eb2fa823
Scott Mielcarski <scott_mielcarski@me.com>
2015-05-25 12:47:09 -0700

    Fix mail not sending properly in production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: "bugtrackerapplication",
-    password: ENV["EMAIL_PASSWORD"],
-    authentication: "plain",
-    enable_starttls_auto: true
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["EMAIL_USERNAME"],
+  password: ENV["EMAIL_PASSWORD"],
+  authentication: :login
 }
